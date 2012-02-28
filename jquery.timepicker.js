703d41b7b0ad651b4cd8ae3c78915e256470cf87
Sam Sehnert <sam@teamdf.com>
2012-02-28 19:25:32 +1300

    Fixed docs syntax hilighting and added license

--- jquery.timepicker.js
+++ jquery.timepicker.js
@@ -1,9 +1,25 @@
-/* 
- * 	jQuery timepicker plug-in 1.0.0
- *	Copyright (C) 2011  Digital Fusion
- *	
+/** 
+ * jQuery timepicker plug-in 1.0.0
+ * Copyright (C) 2012 Digital Fusion
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Sam Sehnert
  */
+ 
 (function($){
+	"use strict";
 	
 	// The name of your plugin. This is used to namespace your
 	// plugin methods, object data, and registerd events.
